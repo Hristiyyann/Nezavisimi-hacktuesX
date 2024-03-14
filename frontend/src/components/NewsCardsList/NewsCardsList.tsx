979cdcf74ef4899b29f0341a36274d6381067cee
Християн Радев <hristiyan.rradev04@abv.bg>
2024-03-14 22:08:49 +0200

    add skeleton to news cards

--- frontend/src/components/NewsCardsList/NewsCardsList.tsx
+++ frontend/src/components/NewsCardsList/NewsCardsList.tsx
@@ -1,12 +1,36 @@
 import NewsCard from "components/NewsCard/NewsCard";
 import classes from './style.module.less';
+import { Skeleton } from "antd";
+import { useState } from "react";
 
 function NewsCardsList() {
+    const [loading, setLoading] = useState(false);
+
     return (
         <div className = {classes.list}>
-            <NewsCard/>
-            <NewsCard/>
-            <NewsCard/>
+        {
+            loading
+            ?
+            <>
+                <Skeleton 
+                    loading 
+                    active 
+                    className = {classes.skeleton}
+                />
+
+                <Skeleton 
+                    loading 
+                    active 
+                    className = {classes.skeleton}
+                />
+            </>
+            :
+            <>
+                <NewsCard/>
+                <NewsCard/>
+                <NewsCard/>
+            </>
+        }
         </div>
     )
 }
